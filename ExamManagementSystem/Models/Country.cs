45215eaba911f0d5e48fd0041ae511961d370a5e
maklesuddin <maklesuddin88@gmail.com>
2018-09-30 01:47:24 +0600

    workdone

---
+++ ExamManagementSystem/Models/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Display(Name = "Country Name")]
+        public string Name { get; set; }
+        public List<City> Cities { get; set; }
+        public List<Trainer> Trainers { get; set; }
+        public List<Participant> Participants { get; set; }
+    }
+}
